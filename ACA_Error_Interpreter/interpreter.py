2a2fc73c1c27d5c4284dfcdb43f89fc64d17b167
RaphaelG76 <ravisityy@gmail.com>
2024-02-16 19:06:49 -0800

    Update interpreter.py

--- ACA_Error_Interpreter/interpreter.py
+++ ACA_Error_Interpreter/interpreter.py
@@ -7,173 +7,126 @@ import datetime
 import pdfkit
 import ctypes
 
+# Constants
+XML_NAMESPACE = "{urn:us:gov:treasury:irs:ext:aca:air:"
+PROMPT_TITLE = 'IRS ACA Errors Report'
+CONFIG = pdfkit.configuration(wkhtmltopdf="wkhtmltox/bin/wkhtmltopdf.exe")
 
 def redirect_print_to_file(statement, file_name):
     with open(file_name, 'a') as file:
         print(statement, file=file)
 
-current_folder = os.path.dirname(__file__)  # Get the directory of the current script
-wkhtmltopdf_path = os.path.join(current_folder, 'wkhtmltox', 'bin', 'wkhtmltopdf.exe')
+def prompt_file(title):
+    root = tk.Tk()
+    root.withdraw()
+    ctypes.windll.user32.MessageBoxW(0, title, PROMPT_TITLE, 0)
+    return filedialog.askopenfilename()
 
-config = pdfkit.configuration(
-    wkhtmltopdf=wkhtmltopdf_path)
+def extract_error_details(root, year):
+    euser_id_list = []
+    euser_error_list = []
+    company_error_list = []
+    sub_status = ''
 
-prompt_users = 'Select Request File'
-prompt_errors = 'Select Acknowledgement File'
-title = 'IRS ACA Errors Report'
+    for form_detail in root.find(f"{XML_NAMESPACE}{year}ACATransmitterSubmissionDetail"):
+        if form_detail.tag == f"{XML_NAMESPACE}{year}TransmitterErrorDetailGrp":
+            if form_detail.find(f"{XML_NAMESPACE}{year}UniqueRecordId") is not None:
+                euser_id_raw = form_detail.find(f"{XML_NAMESPACE}{year}UniqueRecordId")
+                euser_id_text = euser_id_raw.text
+                euser_id = euser_id_text.split('|')[2]
+                euser_error = form_detail[1][1].text
+                euser_id_list.append(euser_id)
+                euser_error_list.append(euser_error)
+            else:
+                if form_detail.find(f"{XML_NAMESPACE}{year}UniqueSubmissionId") is not None:
+                    if form_detail.find(f"{XML_NAMESPACE}{year}SubmissionLevelStatusCd") is None:
+                        company_error = form_detail[1][1].text
+                        company_error_list.append(company_error)
+                    else:
+                        sub_status = form_detail.find(f"{XML_NAMESPACE}{year}SubmissionLevelStatusCd").text
 
-# prompt for files
-root = tk.Tk()
-root.withdraw()
-ctypes.windll.user32.MessageBoxW(0, prompt_users, title, 0)
-file_path = filedialog.askopenfilename()
-print(file_path)
-output_directory = ('/'.join(file_path.split('/')[:-1]))
-print(output_directory)
-ctypes.windll.user32.MessageBoxW(0, prompt_errors, title, 0)
-file_path2 = filedialog.askopenfilename()
+    return euser_id_list, euser_error_list, company_error_list, sub_status
 
-# extract error file name
-file_name2 = os.path.basename(file_path2)
+def process_users(root, year, euser_id_list, euser_error_list, company_error_list, date, unique_sid, output_directory):
+    euser_dictionary = dict(zip(euser_id_list, euser_error_list))
 
-unique_sid = file_name2.split('_')[1]
-date = file_name2.split('_')[2]
+    for form_detail in root.find(f"{XML_NAMESPACE}{year}Form1094CUpstreamDetail"):
+        if form_detail.tag == f"{XML_NAMESPACE}{year}EmployerInformationGrp":
+            business = form_detail.find(f"{XML_NAMESPACE}{year}BusinessName")
+            business_name = business.find(f"{XML_NAMESPACE}{year}BusinessNameLine1Txt").text
+            output_file_name = f"{business_name} Error Report.txt"
+            output_file_name_html = f"{business_name} Error Report.html"
+            output_file_name_pdf = os.path.join(output_directory, f"{business_name} Error Report.pdf")
+            redirect_print_to_file(f"Company Name: {business_name}\n", output_file_name)
+            redirect_print_to_file(f"Date: {date}\n", output_file_name)
+            redirect_print_to_file(f"Submission ID: {unique_sid}\n", output_file_name)
+            redirect_print_to_file(f"Submission Status: {sub_status}\n", output_file_name)
+            now_ordered = datetime.datetime.now().strftime("%m-%d-%Y, %H:%M:%S")
+            redirect_print_to_file(f"Report Ran: {now_ordered}\n", output_file_name)
+            for cerror in company_error_list:
+                cerror_report = f"Name: {business_name}\nError: {cerror}\n"
+                redirect_print_to_file(cerror_report, output_file_name)
 
-tree_users = ET.parse(file_path)  # list of users
-root_users = tree_users.getroot()
+        if form_detail.tag == f"{XML_NAMESPACE}{year}Form1095CUpstreamDetail":
+            user_id = form_detail.find(f"{XML_NAMESPACE}{year}RecordId").text
+            if user_id in euser_id_list:
+                employee_info = form_detail.find(f"{XML_NAMESPACE}{year}EmployeeInfoGrp")
+                names = employee_info.find(f"{XML_NAMESPACE}{year}OtherCompletePersonName")
+                user_fn = names.find(f"{XML_NAMESPACE}{year}PersonFirstNm").text
+                user_ln = names.find(f"{XML_NAMESPACE}{year}PersonLastNm").text
+                user_mn = names.find(f"{XML_NAMESPACE}{year}PersonMiddleNm").text if names.find(f"{XML_NAMESPACE}{year}PersonMiddleNm") is not None else ""
+                user_error_report = f"Name: {user_fn} {user_mn} {user_ln}\nRecord ID: {user_id}\nError: {euser_dictionary[user_id]}\n" if user_mn else f"Name: {user_fn} {user_ln}\nRecord ID: {user_id}\nError: {euser_dictionary[user_id]}\n"
+                redirect_print_to_file(user_error_report, output_file_name)
 
-tree_eusers = ET.parse(file_path2)  # list of users with errors
-root_eusers = tree_eusers.getroot()
+def convert_to_html_pdf(output_file_name, output_file_name_html, output_file_name_pdf):
+    with open(output_file_name) as file:
+        text = file.read()
 
-# initialize values
-euser_id_list = []
-euser_error_list = []
-company_error_list = []
-sub_status = ''
-user_id = ''
-output_file_name = ''
-output_file_name_html = ''
-output_file_name_pdf = ''
+    html_text = '<html>\n<body>\n'
+    for line in text.split('\n\n'):
+        html_text += '<div style="display:block; clear:both; page-break-inside:avoid;">'
+        html_text += '<p style="font-family: helvetica;">'
+        for line2 in line.split('\n'):
+            line3 = line2.split(': ')
+            if len(line3) > 1:
+                html_text += f'<span><b>{line3[0]}: </b>{line3[1]}<br></span>'
+        html_text += '</p>\n'
+        html_text += '</div>'
+    html_text += '</body>\n</html>'
 
-# extract namespace year (ts22,ts23,etc.)
-space = str(root_eusers[0])
-start_index = space.find('{') + 1
-end_index = space.find('}')
-numbers_part = space[start_index:end_index]
-element_list = [element.strip() for element in numbers_part.split(':')]
-year = element_list[-1]
-now = datetime.datetime.now()
-now_no_seconds = str(now).split('.')[0]
-now_no_time = now_no_seconds.split(' ')
-now_ordered_split = now_no_time[0].split('-')
-now_ordered = (now_ordered_split[1] + '-' + now_ordered_split[2] + '-' + now_ordered_split[0] + ', ' +
-               now_no_time[1])
+    with open(output_file_name_html, 'w') as file:
+        file.write(html_text)
 
-# iterate through error form
-for index, form_detail in enumerate(
-        root_eusers.find('{urn:us:gov:treasury:irs:ext:aca:air:' + year + '}ACATransmitterSubmissionDetail')):
-    if form_detail.tag == '{urn:us:gov:treasury:irs:ext:aca:air:' + year + '}TransmitterErrorDetailGrp':
-        if form_detail.find('{urn:us:gov:treasury:irs:ext:aca:air:' + year + '}UniqueRecordId') is not None:
-            euser_id_raw = (form_detail.find('{urn:us:gov:treasury:irs:ext:aca:air:' + year + '}UniqueRecordId'))
-            euser_id_text = euser_id_raw.text  # users with error id
-            euser_id = euser_id_text.split('|')[2]
-            euser_error = form_detail[1][1].text  # error details for users with errors (cant use find because of ns2:)
-            euser_id_list.append(euser_id)
-            euser_error_list.append(euser_error)
-        else:
-            if form_detail.find('{urn:us:gov:treasury:irs:ext:aca:air:' + year + '}UniqueSubmissionId') is not None:
-                if form_detail.find('{urn:us:gov:treasury:irs:ext:aca:air:' + year +
-                                    '}SubmissionLevelStatusCd') is None:
-                    company_error = form_detail[1][1].text
-                    company_error_list.append(company_error)
-                else:
-                    sub_status = form_detail.find(
-                        '{urn:us:gov:treasury:irs:ext:aca:air:' + year + '}SubmissionLevelStatusCd').text
+    path = os.path.abspath(output_file_name_html)
+    pdfkit.from_file(path, output_file_name_pdf, configuration=CONFIG)
+    os.remove(output_file_name)
+    os.remove(output_file_name_html)
 
-euser_dictionary = dict(zip(euser_id_list, euser_error_list))
+if __name__ == "__main__":
+    file_path = prompt_file('Select Request File')
+    output_directory = os.path.dirname(file_path)
+    file_path2 = prompt_file('Select Acknowledgement File')
 
-# prints user id's and names from 'users.xml'
-for form_detail in root_users.find('{urn:us:gov:treasury:irs:ext:aca:air:' + year + '}Form1094CUpstreamDetail'):
-    if form_detail.tag == '{urn:us:gov:treasury:irs:ext:aca:air:' + year + '}EmployerInformationGrp':
-        business = (form_detail.find('{urn:us:gov:treasury:irs:ext:aca:air:' + year + '}BusinessName'))
-        business_name = business.find('{urn:us:gov:treasury:irs:ext:aca:air:' + year + '}BusinessNameLine1Txt').text
-        print(business_name)
-        output_file_name = business_name + " Error Report.txt"
-        output_file_name_html = business_name + " Error Report.html"
-        output_file_name_pdf = os.path.join(output_directory, business_name + " Error Report.pdf")
-        redirect_print_to_file("Company Name: " + business_name + "\n", output_file_name)
-        redirect_print_to_file("Date: " + date + "\n", output_file_name)
-        redirect_print_to_file("Submission ID: " + unique_sid + "\n", output_file_name)
-        redirect_print_to_file("Submission Status: " + sub_status + "\n", output_file_name)
-        redirect_print_to_file("Report Ran: " + now_ordered + "\n", output_file_name)
-        for cerror in company_error_list:
-            cerror_report = ("Name: " + business_name + "\n" + "Error: "
-                             + cerror + "\n")
-            print(cerror_report)
-            redirect_print_to_file(cerror_report, output_file_name)
-    if form_detail.tag == '{urn:us:gov:treasury:irs:ext:aca:air:' + year + '}Form1095CUpstreamDetail':
-        if form_detail.find('{urn:us:gov:treasury:irs:ext:aca:air:' + year + '}RecordId') is not None:
-            # variable for user ids
-            user_id = form_detail.find('{urn:us:gov:treasury:irs:ext:aca:air:' + year + '}RecordId').text
+    file_name2 = os.path.basename(file_path2)
+    unique_sid = file_name2.split('_')[1]
+    date = file_name2.split('_')[2]
+    year = file_name2.split('_')[-1].split('.')[0]
 
-        if user_id in euser_id_list:
-            if form_detail.find('{urn:us:gov:treasury:irs:ext:aca:air:' + year + '}EmployeeInfoGrp') is not None:
-                employee_info = form_detail.find('{urn:us:gov:treasury:irs:ext:aca:air:' + year + '}EmployeeInfoGrp')
-                names = employee_info.find('{urn:us:gov:treasury:irs:ext:aca:air:' + year + '}OtherCompletePersonName')
+    tree_users = ET.parse(file_path)
+    root_users = tree_users.getroot()
 
-                # variable for first names
-                user_fn = names.find('{urn:us:gov:treasury:irs:ext:aca:air:' + year + '}PersonFirstNm').text
-                # variable for last names
-                user_ln = names.find('{urn:us:gov:treasury:irs:ext:aca:air:' + year + '}PersonLastNm').text
-                # variable for middle names
-                if names.find('{urn:us:gov:treasury:irs:ext:aca:air:' + year + '}PersonMiddleNm') is not None:
-                    user_mn = names.find('{urn:us:gov:treasury:irs:ext:aca:air:' + year + '}PersonMiddleNm').text
-                    user_error_report = ("Name: " + user_fn + " " + user_mn + " " + user_ln + "\n" + "Record ID: " +
-                                         user_id + "\n" + "Error: "
-                                         + euser_dictionary[user_id] + "\n")
-                    print(user_error_report)
-                    redirect_print_to_file(user_error_report, output_file_name)
-                else:
-                    user_error_report = ("Name: " + user_fn + " " + user_ln + "\n" + "Record ID: " +
-                                         user_id + "\n" "Error: " +
-                                         euser_dictionary[user_id] + "\n")
-                    print(user_error_report)
-                    redirect_print_to_file(user_error_report, output_file_name)
-        else:
-            pass
+    tree_eusers = ET.parse(file_path2)
+    root_eusers = tree_eusers.getroot()
 
-# read text file into text variable
-file = open(output_file_name)
-text = file.read()
-file.close()
+    euser_id_list, euser_error_list, company_error_list, sub_status = extract_error_details(root_eusers, year)
 
-# convert txt to html
-html_text = '<html>\n<body>\n'
-for line in text.split('\n\n'):
-    html_text += '<div style = "display:block; clear:both; page-break-inside:avoid;">'
-    html_text += '<p style = "font-family: helvetica;">'
-    for line2 in line.split('\n'):
-        line3 = line2.split(': ')
-        if len(line3) > 1:
-            html_text += f'<span><b>{line3[0]}: </b>{line3[1]}<br></span>'
-    html_text += '</p>\n'
-    html_text += '</div>'
-html_text += '</body>\n</html>'
+    process_users(root_users, year, euser_id_list, euser_error_list, company_error_list, date, unique_sid, output_directory)
 
-# save the html file
-with open(output_file_name_html, 'w') as file:
-    file.write(html_text)
+    output_file_name = f"{output_directory}/{business_name} Error Report.txt"
+    output_file_name_html = f"{output_directory}/{business_name} Error Report.html"
+    output_file_name_pdf = f"{output_directory}/{business_name} Error Report.pdf"
 
-# convert html to pdf
-path = os.path.abspath(output_file_name_html)
-# converter.convert(f'file:///{path}', output_file_name_pdf)
-pdfkit.from_file(path, output_file_name_pdf, configuration=config)
+    convert_to_html_pdf(output_file_name, output_file_name_html, output_file_name_pdf)
 
-# remove non-pdf forms
-os.remove(output_file_name)
-os.remove(output_file_name_html)
-
-#open pdf
-
-subprocess.Popen([output_file_name_pdf],shell=True)
+    subprocess.Popen([output_file_name_pdf], shell=True)
 
